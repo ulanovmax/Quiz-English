435904f64e517d0499eaf81225776f9207a9089b
Max Ulanov <ulanovmaks29@gmail.com>
2022-05-17 15:47:36 +0300

    create fuctionality of quiz

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ const mainQuestion = document.querySelector('.question'),
       answer2 = answerList.querySelector('.b'),
       answer3 = answerList.querySelector('.c'),
       nextBtn = document.querySelector('.next_btn'),
-      scoreElement = document.querySelector('.score'),
       progressBar = document.querySelector('.indicator'),
-      progressCount = document.querySelector('.progress_count');
+      progressCount = document.querySelector('.progress_count'),
+      resultsPage = document.querySelector('.results'),
+      scoreElement = document.querySelector('.score');
 
 let pageIndex = 0, // Index of page
     questionIndex = 0, // Index of question
@@ -34,8 +35,23 @@ const questions = [
       "haven't liked"
     ],
     correct: 1
+  },
+  {
+    question: 'Susie .... in England for her holiday last winter.',
+    options: [
+      "were",
+      "was",
+      "is"
+    ],
+    correct: 1
   }
 ]
+
+window.addEventListener('load', () => {
+  randowQuestion();
+}) 
+
+console.log((questionIndex + 1 )/ questions.length);
 
   
 // Data output
@@ -47,7 +63,7 @@ function loadContent() {
   answer2.innerHTML = questions[questionIndex].options[1];
   answer3.innerHTML = questions[questionIndex].options[2];
 
-  questionIndex++;
+  pageIndex++;
 
 }
 
@@ -58,7 +74,7 @@ function randowQuestion() {
   let random = Math.floor(Math.random() * questions.length);
   let dublicate = false;
 
-  if (questionIndex == questions.length) {
+  if (pageIndex == questions.length) {
     finish()
   } else {
     if (completed.length > 0) {
@@ -84,11 +100,54 @@ function randowQuestion() {
 
 
 
-window.addEventListener('load', () => {
-  randowQuestion()
-}) 
+answerList.addEventListener('click', e => {
+  const targ = e.target;
+  if (targ && targ.matches('.answer')) {
+    if (targ.dataset.number == questions[questionIndex].correct) {
+      targ.classList.add('correct');
+      score++;
+    } else {
+      targ.classList.add('mistake')
+    }
+  }
+  disabled();
+  answerList.querySelector('.warning').classList.remove('show');
+ 
+})
+
+
+function disabled() {
+  answers.forEach(answer => {
+    answer.classList.add('disabled');
+    if (answer.dataset.number == questions[questionIndex].correct ) {
+      answer.classList.add('correct');
+    }
+  })
+  nextBtn.classList.add('confirm')
+}
+
+function enabled() {
+  answers.forEach(answer => {
+    answer.classList.remove('disabled', 'correct', 'mistake')
+  });
+  nextBtn.classList.remove('confirm');
+}
+
+function validate() {
+  if (!answers[0].classList.contains('disabled')) {
+    answerList.querySelector('.warning').classList.add('show');
+  } else {
+    randowQuestion()
+    enabled()
+  }
+}
+
+nextBtn.addEventListener('click', validate)
 
 function finish() {
-  console.log('finish');
+  resultsPage.classList.add('finish');
+  scoreElement.innerHTML = `${score} / ${questions.length}`;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
